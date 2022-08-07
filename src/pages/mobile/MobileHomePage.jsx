7f86befd5343820b2e0133b80b0fed31d6b8d426
Milos Rujevic <milosrujevic@gmail.com>
2022-08-07 15:19:52 +0200

    Krakow II

--- src/pages/mobile/MobileHomePage.jsx
+++ src/pages/mobile/MobileHomePage.jsx
@@ -1,8 +1,33 @@
 import styled from 'styled-components'
+
 import bg from '../../assets/index_scale_adoption_mobile.webp'
 
+import LandingPage from './LandingPage'
+import QRaftNFTPage from './QRaftNFTPage'
+import { Grid } from  'react-loader-spinner'
+
+import store from '../../state/state'
+
 const MobileHomePage = () => {
+    const spinner = store(state => state.spinner)
+    const phantomWallet = store(state => state.phantomWallet)
+
     return <Container>
+        <Main>
+            {phantomWallet ?
+                <QRaftNFTPage /> :
+                <LandingPage />
+            }
+            {spinner && <SpinnerBG>
+                <Grid
+                    height='250'
+                    width='250'
+                    radius='9'
+                    color='white'
+                    ariaLabel='three-dots-loading'
+                />
+            </SpinnerBG>}
+        </Main>
         <BGImage src={bg} />
     </Container>
 }
@@ -16,9 +41,35 @@ const Container = styled.div`
     text-align: right;
 `
 
+const Main = styled.div`
+    width: 80%;
+    min-height: 100vh;
+    padding: 20px 10%;
+    position: relative;
+    z-index: 3;
+    text-align: center;
+`
+
+const SpinnerBG = styled.div`
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    z-index: 4;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: rgba(0, 0, 0, .9);
+`
+
 const BGImage = styled.img`
-    height: 100vh;
+    width: 70vw;
+    position: absolute;
+    top: 0;
+    right: 0;
     opacity: 0.3;
+    z-index: 2;
 `
 
 export default MobileHomePage
--- src/pages/web/QRPage.jsx
+++ src/pages/web/QRPage.jsx
@@ -118,24 +118,17 @@ const QRGenerator = ({
 }
 
 const QRNFT = ({
-    getNftTokenData,
     qrLink,
     setQRLink,
     generateQRNFT,
     robohashURL,
     createNFT
 }) => {
-    const [NFTs, setNFTs] = useState([])
     const [selectedNFT, setSelectedNFT] = useState(null)
     const [QRGenerated, setQRGenerated] = useState(false)
 
-    const fetchNFTs = async () => {
-        const data = await getNftTokenData()
-
-        console.log('NFT DATA', data)
-
-        if (data) setNFTs(data)
-    }
+    const NFTs = store(state => state.NFTs)
+    const { getNftTokenData } = useWallet()
 
     const handleGenerateQR = async () => {
         await generateQRNFT(selectedNFT)
@@ -160,11 +153,11 @@ const QRNFT = ({
                     <Button onClick={handleGenerateQR}>Add QR Code</Button>
                 </SelectedNFTContainer> :
                 NFTs?.length === 0 ?
-                    <Button onClick={fetchNFTs}>Get NFTs</Button> :
+                    <Button onClick={getNftTokenData}>Get NFTs</Button> :
                     <>
                         <Heading type='small'>Your NFT Gallery</Heading>
                         <Gallery>
-                            {NFTs?.map(NFT => <Item src={NFT.image_uri} onClick={setSelectedNFT.bind(null, NFT)} />)}
+                            {NFTs?.map(NFT => <Item key={NFT.image_uri} src={NFT.image_uri} onClick={setSelectedNFT.bind(null, NFT)} />)}
                         </Gallery>
                     </>
         }
@@ -177,7 +170,7 @@ const QRPage = () => {
     const setRobotGenerated = store(state => state.setRobotGenerated)
     const setSpinner = store(state => state.setSpinner)
 
-    const { connectPhantomWallet, getNftTokenData } = useWallet()
+    const { connectPhantomWallet } = useWallet()
 
     const [page, setPage] = useState(0)
     const [qrLink, setQRLink] = useState('')
@@ -198,6 +191,7 @@ const QRPage = () => {
         }
     
         var request = new XMLHttpRequest()
+
         request.open('GET', imageLink, true)
         request.responseType = 'blob'
         request.onload = function() {
@@ -209,21 +203,8 @@ const QRPage = () => {
                 setQRGenerated(true)
             }
         }
+
         request.send()
-
-        // const templateParams = {
-        //     currentValue: qrLink,
-        //     walletAddress: phantomWallet.publicKey.toString(),
-        //     deliveryAddress: 'bejzik8@gmail.com'
-        // }
-        console.log('EXE DONE')
-
-        // emailjs.send('service_w6acx2m', 'template_0gczmq7', templateParams, 'FsM-UuY5XXpVXOUdZ')
-        //     .then(result => {
-        //         console.log(result.text)
-        //     }, error => {
-        //         console.log(error.text)
-        //     })
     }
 
     const createNFT = () => {
@@ -327,7 +308,6 @@ const QRPage = () => {
             }
             {page === 2 &&
                 <QRNFT
-                    getNftTokenData={getNftTokenData}
                     qrLink={qrLink}
                     setQRLink={setQRLink}
                     generateQRNFT={generateQRNFT}
